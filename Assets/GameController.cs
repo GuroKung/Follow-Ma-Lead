1a058f00f8976b48232067bf061ff7f48b7dd007
smart2538 <worapon.o@ku.th>
2016-05-20 02:19:45 +0700

    add connection to server

---
+++ Assets/GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+	private SocketIOComponent socket;
+	// Use this for initialization
+	void Start () {
+		GameObject go = GameObject.Find("SocketIO");
+		socket = go.GetComponent<SocketIOComponent>();
+		socket.On("NET_AVAILABLE", onConnection);
+		socket.On ("CONNECTED", OnAuthen);
+	}
+
+	public void onConnection(SocketIOEvent e){
+		Debug.Log("is Connected");
+		Debug.Log(e.data.ToString());
+
+		Dictionary<string, string> data = new Dictionary<string, string>();
+		data["name"] = "John Doe";
+		socket.Emit("LOGIN", new JSONObject(data));
+	}
+
+	public void OnAuthen(SocketIOEvent e){
+		Debug.Log(e.data.ToString());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
--- Assets/SocketIO/Scripts/Test/TestSocketIO.cs
+++ Assets/SocketIO/Scripts/Test/TestSocketIO.cs
@@ -1,4 +1,4 @@
-﻿#region License
+#region License
 /*
  * TestSocketIO.cs
  *
@@ -34,7 +34,7 @@ public class TestSocketIO : MonoBehaviour
 {
 	private SocketIOComponent socket;
 
-	public void Start() 
+	public void Start()
 	{
 		GameObject go = GameObject.Find("SocketIO");
 		socket = go.GetComponent<SocketIOComponent>();
@@ -43,7 +43,7 @@ public class TestSocketIO : MonoBehaviour
 		socket.On("boop", TestBoop);
 		socket.On("error", TestError);
 		socket.On("close", TestClose);
-		
+
 		StartCoroutine("BeepBoop");
 	}
 
@@ -51,22 +51,22 @@ public class TestSocketIO : MonoBehaviour
 	{
 		// wait 1 seconds and continue
 		yield return new WaitForSeconds(1);
-		
+
 		socket.Emit("beep");
-		
+
 		// wait 3 seconds and continue
 		yield return new WaitForSeconds(3);
-		
+
 		socket.Emit("beep");
-		
+
 		// wait 2 seconds and continue
 		yield return new WaitForSeconds(2);
-		
+
 		socket.Emit("beep");
-		
+
 		// wait ONE FRAME and continue
 		yield return null;
-		
+
 		socket.Emit("beep");
 		socket.Emit("beep");
 	}
@@ -75,7 +75,7 @@ public class TestSocketIO : MonoBehaviour
 	{
 		Debug.Log("[SocketIO] Open received: " + e.name + " " + e.data);
 	}
-	
+
 	public void TestBoop(SocketIOEvent e)
 	{
 		Debug.Log("[SocketIO] Boop received: " + e.name + " " + e.data);
@@ -88,14 +88,14 @@ public class TestSocketIO : MonoBehaviour
 			"#####################################################"
 		);
 	}
-	
+
 	public void TestError(SocketIOEvent e)
 	{
 		Debug.Log("[SocketIO] Error received: " + e.name + " " + e.data);
 	}
-	
+
 	public void TestClose(SocketIOEvent e)
-	{	
+	{
 		Debug.Log("[SocketIO] Close received: " + e.name + " " + e.data);
 	}
 }
