ad276093c01b0d3fa6f863b4e11400095f763884
smart2538 <worapon.o@ku.th>
2016-05-26 03:28:18 +0700

    add functional testing dance script

--- Assets/Anim/movetMentController.cs
+++ Assets/Anim/movetMentController.cs
@@ -10,47 +10,11 @@ public class movetMentController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            anim.SetInteger("pressKey", 1);
-            anim.SetInteger("pressKey2", 1);
-            anim.SetInteger("PressKey", 1);
-            anim.SetInteger("PressKey2", 1);
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            anim.SetInteger("pressKey", 2);
-            anim.SetInteger("pressKey2", 2);
-            anim.SetInteger("PressKey", 2);
-            anim.SetInteger("PressKey2", 2);
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            anim.SetInteger("pressKey", 3);
-            anim.SetInteger("pressKey2", 3);
-            anim.SetInteger("PressKey", 3);
-            anim.SetInteger("PressKey2", 3);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            anim.SetInteger("pressKey", 4);
-            anim.SetInteger("pressKey2", 4);
-            anim.SetInteger("PressKey", 4);
-            anim.SetInteger("PressKey2", 4);
-        }
-        if (Input.GetKeyDown(KeyCode.RightControl))
-        {
-            anim.SetInteger("pressKey", 5);
-            anim.SetInteger("pressKey2", 5);
-            anim.SetInteger("PressKey", 5);
-            anim.SetInteger("PressKey2", 5);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            anim.SetInteger("pressKey", 6);
-            anim.SetInteger("pressKey2", 6);
-            anim.SetInteger("PressKey", 6);
-            anim.SetInteger("PressKey2", 6);
-        }
+        
+    }
+
+    public void setDance(int i)
+    {
+        anim.SetInteger("PressKey", i);
     }
 }
---
+++ Assets/Class/ArrayObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+// Use this for initialization
+[Serializable]
+public class ArrayObject
+{
+    public int[] lead_dances;
+
+    public static ArrayObject createFromJson(string json)
+    {
+
+        ArrayObject info = JsonUtility.FromJson<ArrayObject>(json);
+        return info;
+    }
+}
+
