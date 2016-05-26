692815628bc5738734110ff4e1502d24ab175f34
Kittinan Napapongsa <kitty_npp@hotmail.com>
2016-05-26 20:07:16 +0700

    moveCon

--- Assets/Anim/movetMentController.cs
+++ Assets/Anim/movetMentController.cs
@@ -1,20 +1,28 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class movetMentController : MonoBehaviour {
     Animator anim;
+    List<int> dance = new List<int>();
+    int firstElement;
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        
-    }
+        firstElement = dance.First();
+            if (anim.GetInteger("PressKey") == 0 && firstElement!=null)
+            {
+                anim.SetInteger("PressKey", firstElement);
+                dance.Remove(firstElement);
+            }
+        }
 
     public void setDance(int i)
     {
-        anim.SetInteger("PressKey", i);
+        dance.Add(i);
     }
 }
