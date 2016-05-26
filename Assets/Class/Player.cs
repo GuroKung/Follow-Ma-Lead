f473a04e822207ad9c6bb8606389240decbdc77c
smart2538 <worapon.o@ku.th>
2016-05-26 19:53:23 +0700

    add dance two way function

---
+++ Assets/Class/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+// Use this for initialization
+[Serializable]
+public class Player
+{
+    public string id;
+    public bool isLead;
+    public int chId;
+    public bool isUser;
+
+
+    public Player(string id, bool isLead, int ch, bool isUser)
+    {
+        this.id = id;
+        this.isLead = isLead;
+        this.chId = ch;
+        this.isUser = isUser;
+    }
+}
+
---
+++ Assets/Class/checkDanceobject.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System;
+
+// Use this for initialization
+[Serializable]
+public class checkDanceobject
+{
+    public int[] player1_dance;
+    public int[] player2_dance;
+    public bool[] answer;
+    public bool isEnd;
+    public static checkDanceobject createFromJson(string json)
+    {
+
+        checkDanceobject info = JsonUtility.FromJson<checkDanceobject>(json);
+        return info;
+    }
+}
+
